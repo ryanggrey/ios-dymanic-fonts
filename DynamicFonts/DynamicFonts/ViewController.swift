5e76c4d273b1532dec63282072b98f85c4d0480a
Ryan Grey <rgrey@scottlogic.co.uk>
2018-10-15 16:40:41 +0100

    Add support for dynamic font to custom font label

--- DynamicFonts/DynamicFonts/ViewController.swift
+++ DynamicFonts/DynamicFonts/ViewController.swift
@@ -1,20 +1,21 @@
-//
-//  ViewController.swift
-//  DynamicFonts
-//
-//  Created by Ryan Grey on 15/10/2018.
-//  Copyright © 2018 scottlogic. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var customFontLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        customFontLabel.font = genericResizableFont()
     }
 
-
+    private func genericResizableFont() -> UIFont {
+        let categoryLargeSize: CGFloat = 17
+        guard let customFont = UIFont(name: "Rockwell", size: categoryLargeSize) else {
+            return UIFont.systemFont(ofSize: categoryLargeSize);
+        }
+        
+        return UIFontMetrics.default.scaledFont(for: customFont)
+    }
 }
 
